61854a55ea297c7463d1270b337655dde15faac4
danieltheil <56281274+danieltheil@users.noreply.github.com>
2020-02-05 18:20:06 +0100

    Create uwufy.js

---
+++ uwufy.js
@@ -0,0 +1,37 @@
+function uwufy(string) {
+    var owoString = "";
+    for (var char of string) {
+        switch (char) {
+            case 'l':
+                char = 'w';
+                break;
+            case 'r':
+                char = 'w';
+                break;
+            case 'v':
+                char = 'w';
+                break;
+        }
+        owoString += char;
+    }
+    owoString += " ~ uwu";
+    return owoString;
+}
+// token to login
+const token = require('botToken.json');
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('message', (message) => {
+    if (message.isMentioned(bot.user)) {
+        // gets rid of <[userID]> => message.content = inputMessage of user
+        message.content = message.content.slice(message.content.indexOf(">") + 2, message.content.length);
+        if (message.content.startsWith("!padoru")) {
+            message.channel.send("HASHIRE SORI YOKAZE NO YOU NITSUKIMIHARA WOPADORU PADORU");
+            return;
+        }
+        message.channel.send(uwufy(message.content));
+    }
+});
+
+bot.login(token.token);
