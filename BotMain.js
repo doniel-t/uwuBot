4d540d5799a7b0a2c0149e906782992e273d24c7
danieltheil <56281274+danieltheil@users.noreply.github.com>
2020-02-05 23:18:08 +0100

    Added maplink

--- BotMain.js
+++ BotMain.js
@@ -11,7 +11,7 @@ const osuAPI = new osu.Api(osuAPIKey.key, {
 });
 
 function osuRecent() {
-    
+
 }
 
 async function getRecent(message) {
@@ -21,30 +21,27 @@ async function getRecent(message) {
     }
 
     s = osuAPI.getUserRecent({ u: contentArgs[1] }).then( //osuAPI-Call
-        async result => { 
+        async result => {
 
-        recentScore= result[0];
+            recentScore = result[0];
 
-        var Acc = recentScore.accuracy * 100;
+            var Acc = recentScore.accuracy * 100;
 
-        let endMessage =            "Score:    "           .concat(recentScore.score)
-                                    .concat("\nCombo:    ").concat(recentScore.maxCombo)
-                                    .concat("\nTitle:    ").concat(recentScore.beatmap.title)
-                                    .concat("\nDiff:     ").concat(recentScore.beatmap.version)
-                                    .concat("\nStarDiff: ").concat(recentScore.beatmap.difficulty.rating)
-                                    .concat("\nBPM:      ").concat(recentScore.beatmap.bpm)
-                                    .concat("\nAcc:      ").concat(Acc).concat("%");
-        ;
+            let endMessage = "Score:    ".concat(recentScore.score)
+                .concat("\nCombo:    ").concat(recentScore.maxCombo)
+                .concat("\nTitle:    ").concat(recentScore.beatmap.title)
+                .concat("\nLink:     ").concat("https://osu.ppy.sh/beatmapsets/").concat(recentScore.beatmap.beatmapSetId)
+                .concat("\nDiff:     ").concat(recentScore.beatmap.version)
+                .concat("\nStarDiff: ").concat(recentScore.beatmap.difficulty.rating)
+                .concat("\nBPM:      ").concat(recentScore.beatmap.bpm)
+                .concat("\nAcc:      ").concat(Acc).concat("%");
+            console.log(message.author);
 
-        if(recentScore.pp != null) {
-            endMessage = endMessage .concat("\nPP:       ").concat(recentScore.pp);
+            if (recentScore.pp != null) {
+                endMessage = endMessage.concat("\nPP:       ").concat(recentScore.pp);
+            }
+            return endMessage;
         }
-        
-        // for (var mod in recentScore.mods) {
-        //     endMessage += mod.toString();
-        // }
-        return endMessage;
-    }
     ).catch(error => {
         console.log(error);
     });
@@ -101,11 +98,13 @@ bot.on('message', (message) => { //Grab Message
         message.channel.send(uwufyMessage(message, contentArgs));
     }
 
-    if(message.content.startsWith('!osu')) {        //Here all osu! related Code
-        if (contentArgs[0] === ('!osurecent')) {
+    if (message.content.startsWith('!osu')) { //Here all osu! related Code
+        if (contentArgs[0] === '!osurecent') {
             getRecent(message);
         }
     }
+
+
 });
 
 bot.login(token.token);
