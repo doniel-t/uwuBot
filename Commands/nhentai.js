f4a1ead8c076dc36d0664d5fa1903c3411adc6c8
56281274 <theil-daniel@gmx.de>
2020-02-06 20:59:58 +0100

    Added random search in nhentai

--- Commands/nhentai.js
+++ Commands/nhentai.js
@@ -1,6 +1,17 @@
+const nHentaiApi = require('nhentai-api-js')
+let api = new nHentaiApi();
+
 module.exports = {
     nhentai: function(message) {
         let contentArgs = message.content.split(" "); //Split Message for simpler Access
-        message.channel.send("https://nhentai.net/g/".concat(contentArgs[1]));
+        if (contentArgs[1] === '' || contentArgs[1] == null) {
+            let randomId = '';
+            let randomPromise = api.random().then(async result => {
+                randomId = await result.id;
+                message.channel.send("https://nhentai.net/g/".concat(randomId));
+            });
+        } else {
+            message.channel.send("https://nhentai.net/g/".concat(contentArgs[1]));
+        }
     }
 }
