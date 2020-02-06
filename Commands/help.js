68d966e5ded7866244d57ae528230ad44880244b
56281274 <theil-daniel@gmx.de>
2020-02-06 23:49:04 +0100

    updated help

--- Commands/help.js
+++ Commands/help.js
@@ -8,6 +8,7 @@ module.exports = {
             .concat("!nhentaiprint <optional: Number>              => prints every page of a random(or your chosen) nhentai hentai\n")
             .concat("!osuplays <optional: osuName>                  => gives you your(or osuName's) 5 best osu plays\n")
             .concat("!osurecent <optional: osuName>                => gives you your (or osuName's) most recent play(passed or not passed\n")
-            .concat("!leaguegetgame <optional: leagueName> => gives you your (or leagueName's) current league game\n"));
+            .concat("!leaguegetgame <optional: leagueName> => gives you your (or leagueName's) current league game\n")
+            .concat("!poll <TITLE optional: option 1 ...>                => gives you a poll based on your TITLE (optional: with custom options)\n"));
     }
 }
