58e025b5f4ba43f86607fd742307a0cceb9d2dcb
56281274 <theil-daniel@gmx.de>
2020-02-06 23:30:21 +0100

    Added !help

---
+++ Commands/help.js
@@ -0,0 +1,13 @@
+module.exports = {
+    help: function(message) {
+        message.channel.send("List of current commands: \n"
+            .concat("!padoru                                                             => writes padoru!\n")
+            .concat("!ask <QUESTION>                                         => answers your question with 100% accuracy\n")
+            .concat("!uwufy <MESSAGE>                                      => uwufies your message\n")
+            .concat("!nhentai <optional: Number>                       => gives you a link to a random(or your chosen)nhentai hentai\n")
+            .concat("!nhentaiprint <optional: Number>              => prints every page of a random(or your chosen) nhentai hentai\n")
+            .concat("!osuplays <optional: osuName>                  => gives you your(or osuName's) 5 best osu plays\n")
+            .concat("!osurecent <optional: osuName>                => gives you your (or osuName's) most recent play(passed or not passed\n")
+            .concat("!leaguegetgame <optional: leagueName> => gives you your (or leagueName's) current league game\n"));
+    }
+}
