785f0b355d657fe65d08dc9684e74c88d3d58b91
56281274 <theil-daniel@gmx.de>
2020-02-10 15:16:21 +0100

    added id in logger.js

--- Logger.js
+++ Logger.js
@@ -2,16 +2,16 @@ const fs = require('fs');
 const LogFile = 'Bot.log';
 
 module.exports = {
-    log: function (error) {
+    log: function(error) {
         console.log(error);
     },
 
-    get: function (message) {
+    get: function(message) {
         message.channel.send("Clear Log manual", { files: ["./Bot.log"] });
     },
 
     isAdmin: function(message) {
-        if (message.author.id == '270929192399536138' || 'THEILS ID HERE') {
+        if (message.author.id == '270929192399536138' || '222398053703876628') {
             return true;
         } else {
             console.log(message.author.username + " executed an Admin command");
--- botMain.js
+++ botMain.js
@@ -10,46 +10,47 @@ bot.on('message', (message) => { //Grab Message
 
     let contentArgs = message.content.split(" "); //Split Message for simpler Access
 
-    if (contentArgs[0].charAt(0) == '!') {   //Call Method
+    Logger.log(message);
+    if (contentArgs[0].charAt(0) == '!') { //Call Method
 
         let command = contentArgs[0].substring(1);
         command = command.concat('.').concat(command);
         try {
-          executeFunctionByName(command,commands,message); //Calls function
-        }catch(error) {
+            executeFunctionByName(command, commands, message); //Calls function
+        } catch (error) {
             Logger.log(error);
             message.channel.send('Command not Found, use !help for help');
         }
-        
+
         //So you have to call the .js and your function like the command you want to execute at.
         //Example !osurecent calls commands.osurecent.osurecent(message)
     }
 
-    if(contentArgs[0].startsWith('@UwU_Bot')) {
-      try {
-        if (Logger.isAdmin(message)) {
-          executeFunctionByName(contentArgs[1],Logger,message);
-        } else {
-          message.channel.send('You are not an Admin');
+    if (contentArgs[0].startsWith('@UwU_Bot')) {
+        try {
+            if (Logger.isAdmin(message)) {
+                executeFunctionByName(contentArgs[1], Logger, message);
+            } else {
+                message.channel.send('You are not an Admin');
+            }
+
+        } catch (error) {
+            Logger.log(error);
+            message.channel.send('Not a admin command');
         }
-        
-      }catch(error) {
-        Logger.log(error);
-        message.channel.send('Not a admin command');
-      }
-      
+
     }
 
 });
 
 bot.login(token.token); //Starts Bot
 
-function executeFunctionByName(functionName, context /*, args */) {
+function executeFunctionByName(functionName, context /*, args */ ) {
     var args = Array.prototype.slice.call(arguments, 2);
     var namespaces = functionName.split(".");
     var func = namespaces.pop();
-    for(var i = 0; i < namespaces.length; i++) {
-      context = context[namespaces[i]];
+    for (var i = 0; i < namespaces.length; i++) {
+        context = context[namespaces[i]];
     }
     return context[func].apply(context, args);
-  }
+}
