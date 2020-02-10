8d9ff27fa48eb35799617a289b9126a7289f3225
56281274 <theil-daniel@gmx.de>
2020-02-10 21:34:08 +0100

    fixed cmd on server

--- Admin.js
+++ Admin.js
@@ -5,16 +5,16 @@ const { spawn } = require('child_process');
 
 
 module.exports = {
-    log: function (error) {
+    log: function(error) {
         console.log(error);
         console.log('\n-------------------------------------------- \n\n');
     },
 
-    get: function (message) {
+    get: function(message) {
         message.channel.send("Clear Log manual", { files: ["./Bot.log"] });
     },
 
-    isAdmin: function (message) {
+    isAdmin: function(message) {
         if (Admins.includes(message.author.id)) {
             return true;
         } else {
@@ -23,7 +23,7 @@ module.exports = {
         }
     },
 
-    searchUpdate: function (message) {
+    searchUpdate: function(message) {
 
         var url = 'https://raw.githubusercontent.com/danieltheil/uwuBot/master/Files/version.json';
         var jsonFile = new XMLHttpRequest();
@@ -32,7 +32,7 @@ module.exports = {
 
         var gitversion = '';
 
-        jsonFile.onreadystatechange = function () {
+        jsonFile.onreadystatechange = function() {
             if (jsonFile.readyState == 4 && jsonFile.status == 200) {
 
                 gitversion = JSON.parse(jsonFile.responseText);
@@ -47,13 +47,13 @@ module.exports = {
         }
     },
 
-    applyUpdate: function (message) {
+    applyUpdate: function(message) {
         if (!Updateable) {
             message.channel.send('Please search for Update first');
 
         } else {
-            let pro = spawn('start', ['cmd.exe','/k', 'Updater.bat'], { shell: true });
-        
+            let pro = spawn('start', ['cmd.exe', '/c', 'Updater.bat'], { shell: true });
+
             pro.on('exit', m => {
                 console.log(m);
                 process.exit(0);
@@ -64,29 +64,29 @@ module.exports = {
 
     stop: function(message) {
         if (stopvar) {
-            process.exit(0); 
+            process.exit(0);
         } else {
             message.channel.send("If you really want to stop the Bot call this function again within 10 sec");
             stopvar = true;
-            setTimeout(function() {stopvar = false;},10000);
+            setTimeout(function() { stopvar = false; }, 10000);
         }
     },
 
     restart: function(_message) {
 
-        let pro2 = spawn('start', ['cmd.exe','/k', 'run.bat'], { shell: true });
+        let pro2 = spawn('start', ['cmd.exe', '/c', 'run.bat'], { shell: true });
 
         pro2.on('exit', m => {
             console.log(m);
             process.exit(0);
         })
-        
+
     }
 }
 
-var Admins = [              //Add DiscordID for AdminAccess
-    '270929192399536138',   //ackhack
-    '222398053703876628'    //Human Daniel
+var Admins = [ //Add DiscordID for AdminAccess
+    '270929192399536138', //ackhack
+    '222398053703876628' //Human Daniel
 ]
 
 var stopvar = false;
