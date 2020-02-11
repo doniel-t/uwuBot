9e5f28ed36068274187186cd9f0592d6d34946d0
ackhack <christoph.feurbacher@gmx.de>
2020-02-11 19:38:09 +0100

    added ransub

---
+++ Commands/ransub.js
@@ -0,0 +1,30 @@
+const Logger = require('../Admin.js');
+const snoowrap = require('snoowrap');
+const apiKey = require('../Dependencies/RedditAPI.json');
+const redditAPI = new snoowrap({
+    userAgent: 'my user-agent',
+    clientId: apiKey.clientId,
+    clientSecret: apiKey.clientSecret,
+    username: apiKey.username,
+    password: apiKey.password
+});
+
+module.exports = {
+
+    ransub: function (message) {
+
+        let contentArgs = message.content.split(" "); //Split Message for simpler Access
+
+        redditAPI.getSubreddit(contentArgs[1]).getRandomSubmission().then(submission => {
+            if (submission.permalink == undefined) {
+                message.channel.send('Reddit returned undefined (can happen for some subreddit`s, idk why)');
+            } else {
+                message.channel.send("http://reddit.com" + submission.permalink);
+            }
+
+        }).catch(error => {
+            Logger.log(error);
+            message.channel.send("An Error occured");
+        })
+    }
+}
