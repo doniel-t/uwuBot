66212da327660f233a1081b73bab2655ef3d78b8
56281274 <theil-daniel@gmx.de>
2020-02-17 14:11:36 +0100

    fixed typo

--- Commands/hangman.js
+++ Commands/hangman.js
@@ -34,7 +34,7 @@ var listener = function(inputLetter) { //Controls game state
     if (inputLetter.channel == ogMessage.channel) {
         if (inputLetter.content.length === 1) {
             checkLetter(inputLetter.content);
-            ogMessage.channel.send(hiddenMessage + " 11tries left" + hp);
+            ogMessage.channel.send(hiddenMessage + " tries left" + hp);
             if (hp === 0) {
                 ogMessage.channel.send("You lose xD " + inputWord);
                 hp = 7;
