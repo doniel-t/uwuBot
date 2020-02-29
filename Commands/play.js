93464ac613bf6b358a489e1cea35398fc57719d6
ackhack <christoph.feurbacher@gmx.de>
2020-02-29 15:38:17 +0100

    1.9.7

--- Commands/play.js
+++ Commands/play.js
@@ -3,6 +3,23 @@ const Music = require('./Music.js');
 module.exports = {
     play: function (message, bot) {
 
-        Music.play(message,bot); //All Logic is in Music
+        switch (message.contents) { //AutoComplete for certain KeyWords
+            case 'akari':
+                message.contents = '!play https://www.youtube.com/watch?v=3EEtoN8KOSI';
+                break;
+            case 'ayaya':
+                message.contents = '!play https://www.youtube.com/watch?v=D0q0QeQbw9U';
+                break;
+            case 'bruh':
+                message.contents = '!play https://www.youtube.com/watch?v=2ZIpFytCSVc';
+                break;
+            case 'john':
+                message.contents = '!play https://www.youtube.com/watch?v=__owglEs2qE';
+                break;
+            default:
+                break;
+        }
+
+        Music.play(message, bot); //All Logic is in Music
     }
 }
