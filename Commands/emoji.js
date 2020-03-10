3c2da7ce53c2934361d2c5ef274383977a3ad794
ackhack <christoph.feurbacher@gmx.de>
2020-03-10 02:34:28 +0100

    2.1.1

--- Commands/emoji.js
+++ Commands/emoji.js
@@ -3,6 +3,11 @@ module.exports = {
 
         let contentArgs = message.content.split(" "); //Split Message for simpler Access
 
+        if (contentArgs[1] == 'all') {
+            printAll(message,bot);
+            return;
+        }
+
         var emoji = bot.emojis.find(e => e.name == contentArgs[1]);   //get Emoji from Server
 
         if (emoji == null) { //Error detection
@@ -10,15 +15,31 @@ module.exports = {
             return;
         }
 
-        var emojiname = '';
-
-        if (emoji.animated) { //Difference between animated and still
-            emojiname = '<a:';
-        } else {
-            emojiname = '<:';
-        }
-
-        emojiname = emojiname + emoji.name + ':' + emoji.id + '>'; //Build emojiString
-        message.channel.send(emojiname); //Send Message
+        message.channel.send(getEmojiString(emoji)); //Send Message
     }
+}
+
+function getEmojiString(emoji) {
+    var emojiname = '';
+
+    if (emoji.animated) { //Difference between animated and still
+        emojiname = '<a:';
+    } else {
+        emojiname = '<:';
+    }
+
+    emojiname = emojiname + emoji.name + ':' + emoji.id + '>'; //Build emojiString
+    return emojiname;
+}
+
+function printAll(message,bot) {
+    var sendMessage = '';
+    for (var val of bot.emojis) {
+        sendMessage = sendMessage.concat(getEmojiString(val[1]));
+        if (sendMessage.length > 1800) {
+            message.channel.send(sendMessage); //Send Message
+            sendMessage = '';
+        }      
+    }
+    message.channel.send(sendMessage);
 }
