666e841792ff28b1a70aa8f31bb51515dd7434d2
ackhack <christoph.feurbacher@gmx.de>
2020-03-10 13:30:06 +0100

    2.1.3

--- Commands/emoji.js
+++ Commands/emoji.js
@@ -4,7 +4,7 @@ module.exports = {
         let contentArgs = message.content.split(" "); //Split Message for simpler Access
 
         if (contentArgs[1] == 'all') {
-            printAll(message,bot);
+            printAll(message, bot);
             return;
         }
 
@@ -18,26 +18,37 @@ module.exports = {
         message.channel.send(getEmojiString(emoji)); //Send Message
     },
 
-    emojiDetection(message,bot) {
-        let contentArgs = message.content.replace(/:/g,' ').split(" "); //Replace : with Space and split Message
+    emojiDetection(message, bot) {
+
+        let contentArgs = message.content.replace(/:/g, ' ').split(" "); //Replace : with Space and split Message
 
         if (contentArgs[0] == '!e' || contentArgs[0] == '!emoji') {
             contentArgs.shift(); //Remove first emoji and command if !emoji / !e is called
-            contentArgs.shift(); 
+            contentArgs.shift();
         }
+
+        var counter = 0;
+        var sendMessage = '';
 
         for (var word of contentArgs) {
-            if (word[0] == ':' && word[word.length-1] == ':') { //Remove : if needed
-                word = word.substring(1,word.length-1);
+            if (word[0] == ':' && word[word.length - 1] == ':') { //Remove : if needed
+                word = word.substring(1, word.length - 1);
             }
             let emoji = bot.emojis.find(e => e.name == word);   //Find emoji
+
             if (emoji) {
-                let sendMessage = message;
-                sendMessage.content = '!e ' + emoji.name;
-                this.emoji(sendMessage,bot);
+                sendMessage = sendMessage.concat(getEmojiString(emoji));
+                counter++;
+                if (counter == 27) {
+                    message.channel.send(sendMessage); //Send Message
+                    sendMessage = '';
+                    counter = 0;
+                }
             }
         }
-
+        if (sendMessage.length > 0) {
+        message.channel.send(sendMessage);            
+        }
     }
 }
 
@@ -54,7 +65,7 @@ function getEmojiString(emoji) {
     return emojiname;
 }
 
-function printAll(message,bot) {
+function printAll(message, bot) {
     var sendMessage = '';
     var counter = 0;
     for (var val of bot.emojis) {
@@ -64,7 +75,7 @@ function printAll(message,bot) {
             message.channel.send(sendMessage); //Send Message
             sendMessage = '';
             counter = 0;
-        }      
+        }
     }
     message.channel.send(sendMessage);
 }
