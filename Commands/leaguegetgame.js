5f816f2d820bf825f32e864ae8ceb83e68aae933
ackhack <christoph.feurbacher@gmx.de>
2020-03-13 15:43:11 +0100

    2.2.2

--- Commands/leaguegetgame.js
+++ Commands/leaguegetgame.js
@@ -22,7 +22,7 @@ module.exports = {
                 mes.setTitle('LEAGUE GAME');
 
                 for (var x = 0; x < activeGames.participants.length; x++) {
-                    let Link = '[' + activeGames.participants[x].summonerName + '](' + 'https://euw.op.gg/summoner/userName=' + activeGames.participants[x].summonerName.replace(/ /g,'_') + ')';
+                    let Link = '[' + activeGames.participants[x].summonerName + '](' + 'https://euw.op.gg/summoner/userName=' + activeGames.participants[x].summonerName.replace(/ /g, '_') + ')';
                     mes.addField(getChamp(activeGames.participants[x].championId), Link);
                 }
                 message.channel.send(mes);
@@ -67,6 +67,7 @@ function getleagueName(message) { //Gives back a NameString
                 return "No User given";
         }
     } else {
-        return contentArgs[1]; //When Name given
+        contentArgs.shift();
+        return contentArgs.join().replace(/,/g, ' ');  //When Name given
     }
 }
