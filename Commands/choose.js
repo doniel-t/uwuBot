2146288f83720f0a57640e490921c45a6a0d8244
56281274 <theil-daniel@gmx.de>
2020-03-14 19:41:49 +0100

    3.0.1

--- Commands/choose.js
+++ Commands/choose.js
@@ -2,7 +2,7 @@
         choose: function(message) {
             try {
                 let contentArgs = message.content.substring(message.content.indexOf(' ') + 1).split(",");
-                message.channel.send(contentArgs[(Math.round(((contentArgs.length - 1) * Math.random())))]);
+                message.channel.send(contentArgs[Math.round((contentArgs.length - 1) * Math.random())]);
             } catch (err) {
                 message.channel.send("Invalid arguments");
             }
