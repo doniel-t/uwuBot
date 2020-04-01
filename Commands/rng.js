aa8ebc1be711eacb36e92e4142262d5304028a89
56281274 <theil-daniel@gmx.de>
2020-04-01 04:36:50 +0200

    3.6.0

--- Commands/rng.js
+++ Commands/rng.js
@@ -1,13 +1,17 @@
 module.exports = {
     rng: function(message) {
-        let contentArgs = message.split(" ");
+        let contentArgs = message.content.split(" ");
         let min = parseInt(contentArgs[1]);
         let max = parseInt(contentArgs[2]);
 
+        if (min < 0 || max < 0) {
+            message.channel.send("Please use numbers >= 0");
+            return;
+        }
         if (min > max) {
             message.channel.send("Your min value was bigger than max value");
-        }
-        if (isNan(min) || isNaN(max))
+        } else {
             message.channel.send(Math.round(Math.random() * (max - min)) + min);
+        }
     }
 }
