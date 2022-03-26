d000b5cbe9da30398ec23f7ef026f69b82eeb72f
danieltheil <danieltheil>
2022-03-26 03:29:45 +0100

    Added April Fools joke for ask

--- Commands/ask.js
+++ Commands/ask.js
@@ -12,7 +12,7 @@ module.exports = {
             "As I see it, yes.",
             "Don't count on it",
             "It is certain",
-            "It is decidedly so.",
+            "Affirmative",
             "Most likely",
             "My reply is no",
             "My sources say no.",
@@ -25,8 +25,59 @@ module.exports = {
             "Yes - definitely.",
             "You may rely on it"
         ];
+
+        const aprilfoolsAnswers = [
+            "Ey du Hurensohn",
+            "I hope you lose your next 50/50",
+            "XD IMAGINE THINKING THAT",
+            "you are the bot now",
+            "KEKW",
+            "LMAO YOU STOOPID",
+            "bruh fr?",
+            "you are lowkey mid",
+            "you are lowkey midly retarded",
+            "deez nuts",
+            "trump looking ass XD",
+            "iq diff XD",
+            "gura deadass dog tier",
+            "your opinion is trash",
+            "go cry in a corner fgt",
+            "my man deadass thinks the sun is a planet",
+            "fucking donkey",
+            "monke brain ask",
+            "??????",
+            "L + Ratio",
+            "No bitches ????",
+            "didnt ask",
+            "get gud",
+            "try coping harder retard",
+            "no parents ??",
+            "4chan brain",
+            "^ negativ iq",
+            "small brain",
+            "boomer brain XD",
+            "tictactoe forehead",
+            "no maidens??",
+            "^ greentext vibes",
+            "french",
+            "..."
+        ]
+
+        let isAprilFools = () => {
+            let aprilFoolsDay = {
+                month: 3,
+                date: 1
+            };
+            let now = new Date();
+            return now.getMonth() == aprilFoolsDay.month && now.getDate() == aprilFoolsDay.date;
+        };
+
+        let messageSendFunc = (answerArray) => {
+            message.channel.send(answerArray[Math.floor(Math.random() * answerArray.length)]);
+        }
+
         try {
-            message.channel.send(Answers[Math.floor(Math.random() * Answers.length)]);
+            isAprilFools() ? messageSendFunc(aprilfoolsAnswers) : messageSendFunc(Answers);
         }catch (err) {
             Logger.log(err);
             message.channel.send("There was a problem, please try again");
